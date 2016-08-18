e87f10583b2c3d76260a04a8581225441ca53d5b
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2016-08-18 21:38:41 +0200

    Update license info in files.

--- lib/mctat2/version.rb
+++ lib/mctat2/version.rb
@@ -1,3 +1,20 @@
+#    This file is part of Mctat2.
+#
+#    Mctat2 is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Mctat2 is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 module Mctat2
+  APP_NAME = 'mctat2'
+  EXTENSION = '.mctat2.json'
   VERSION = "0.1.0"
 end
